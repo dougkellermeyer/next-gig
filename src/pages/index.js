6c7cdf58b31b83bb75acf8f9f0912f505730ad47
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2023-11-20 00:41:10 -0500

    first pass

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,10 +13,10 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Find your Next Job Now" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        <ArticlesSection sectionId="articles" heading="Latest Jos" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
         <InterestsSection sectionId="details" heading="Details" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
